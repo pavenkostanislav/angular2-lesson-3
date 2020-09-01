6b6662b13ce037c5803aee8370704a8cb9c70151
Stanislav Pavenko <pavenko_sv@mail.ru>
2020-09-01 18:49:42 +0300

    Удалил спеки и причесал

--- src/app/my-directive/directives/shark.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SharkDirective } from './shark.directive';
-
-describe('SharkDirective', () => {
-  it('should create an instance', () => {
-    const directive = new SharkDirective();
-    expect(directive).toBeTruthy();
-  });
-});
